8e23d58f6bfff91b4859cfea4243b02d11bd4ede
Barry Nelson <abcbarryn@users.noreply.github.com>
2026-04-24 20:26:27 -0400

    Updated for universal binary

--- Casks/nethack-gui.rb
+++ Casks/nethack-gui.rb
@@ -1,6 +1,6 @@
 cask "nethack-gui" do
   version "3.7.0"
-  sha256 "3e49b57841c60ab7908c49734c0134c7baf2e30d1e3335d0b3d6f4d1f3dd35f5"
+  sha256 "5ac0aaa68e08d6686d5cb6bec12122a4e508a5f9f57a7a6bd731d5207a38a697"
 
   # Replace with the public URL to your DMG file
   url "https://github.com/abcbarryn/NetHack/releases/download/NetHack-3.7.0/NetHack.dmg"
@@ -10,7 +10,7 @@ cask "nethack-gui" do
   
   # Requirement checks
   depends_on macos: ">= :tahoe"
-  depends_on arch: :arm64
+  #depends_on arch: :arm64
   
   # This identifies the .app inside your DMG to move to /Applications
   app "NetHack.app"
